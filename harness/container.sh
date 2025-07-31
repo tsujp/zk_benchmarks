6ef81fb038bd4b56074fece4645de429b16fc8f4
Jordan Ellis Coppard <jc+git@wz.ht>
2025-08-01 05:35:44 +0900

    fix USERNAME argument propagation; add basic run

--- harness/container.sh
+++ harness/container.sh
@@ -15,22 +15,17 @@ make_container ()
 }
 
 
-# TODO: Use underscores in image and container names instead of hyphens jam_foo_lorem instead of jam-foo-lorem.
-# TODO: Rename (as appropriate) as this function should create (if not already exists), and start the container both.
 run_container ()
 {
-    local container_user="$(podman image inspect localhost/jam/"$JAM_NAME" --format "{{.User}}")"
-    # TODO: Error checking on above output exit status and value (not empty string). Also, what are valid linux usernames? Just realised I don't know concretely. Probably a-Z0-9 with underscore and hyphen only? i.e. regex validate as appropriate too.
-
     podman run -it -d --replace \
            --init \
            --userns keep-id \
 	       --security-opt label=disable \
-	       --hostname "jam-$JAM_NAME" \
-	       --name "jam-$JAM_NAME" \
+	       --hostname "benchy-${1}" \
+	       --name "benchy-${1}" \
            --network=host \
            --cap-add=SYS_PTRACE,CAP_PERFMON,CAP_SYS_ADMIN \
            --cpuset-cpus=4 \
            --cpus=0 \
-	       localhost/jam/"$JAM_NAME"
+	       localhost/benchy/"$1"
 }

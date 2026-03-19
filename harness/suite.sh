7b0b791fe2a1bb0e60ce673cae022560baff071a
Jordan Ellis Coppard <jc+git@wz.ht>
2026-03-19 19:59:39 +0900

    remove unused code
    - This approach not used anymore (or for a while).

--- harness/suite.sh
+++ harness/suite.sh
@@ -20,32 +20,3 @@ for ff in suite/*/*.sh; do
     unset -nv suite_ptr
     unset -v suite_name
 done; unset -v ff
-
-# echo "suites: ${!suites[@]}"
-# echo "aztec: ${suite_aztec[@]}"
-# echo "starkware: ${suite_starkware[@]}"
-
-list_enabled ()
-{
-    for i in "${!enabled[@]}"; do
-        puts "    $(( i + 1 ))) ${enabled[$i]}"
-    done
-}
-
-# XXX: For now all fixture definitions (benches to run) are defined centrally, until YSH or Zig rewrite.
-
-declare -Ar fixtures=(
-    'aztec'='aztec_fixtures'
-)
-
-declare -ar aztec_fixtures=(
-    'bench_eddsa_poseidon'
-    'bench_poseidon2_hash'
-    'bench_poseidon2_hash_100'
-    'bench_poseidon2_hash_30'
-    'bench_poseidon_hash'
-    'bench_poseidon_hash_100'
-    'bench_poseidon_hash_30'
-    'semaphore_depth_10'
-    'sha512_100_bytes'
-)
